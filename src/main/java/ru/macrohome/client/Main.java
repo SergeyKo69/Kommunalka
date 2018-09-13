66169f2f0cbf1f667462bd1fb70104d3b652ce9e
Kogut Sergey <koguts@yandex.ru>
2018-09-13 22:53:28 +0300

    main class

---
+++ src/main/java/ru/macrohome/client/Main.java
@@ -0,0 +1,29 @@
+package ru.macrohome.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import ru.macrohome.server.Connector;
+
+public class Main extends Application {
+    public static Stage primaryStage;
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        this.primaryStage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("/payments_MainForm.fxml"));
+        primaryStage.setTitle("Kommunalka");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop(){
+        Connector.closeSessionFactory();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
